3ea128976aaa94d9faf3364dc3cde363530080f5
sdual <shr.nrht.0623@gmail.com>
2023-10-09 00:52:27 +0900

    remove comment

--- apps/serving/server/server.go
+++ apps/serving/server/server.go
@@ -20,16 +20,13 @@ func (gs GRPCServer) Start() {
 		panic(err)
 	}
 
-	// 2. gRPCサーバーを作成
 	s := grpc.NewServer()
 
-	// 3. 作成したgRPCサーバーを、8080番ポートで稼働させる
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
 		s.Serve(listener)
 	}()
 
-	// 4.Ctrl+Cが入力されたらGraceful shutdownされるようにする
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
