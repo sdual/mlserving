05c6eef64915fa957ee306fbf362f65385526bfb
sdual <shr.nrht.0623@gmail.com>
2023-10-16 17:53:57 +0900

    add env to deployment

--- apps/serving/cmd/grpc/main.go
+++ apps/serving/cmd/grpc/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sdual/mlserving/apps/serving/internal/config"
 	"github.com/sdual/mlserving/apps/serving/internal/server"
 	"github.com/sdual/mlserving/pkg/appconf"
@@ -14,7 +12,6 @@ const appName = "serving"
 func main() {
 	appConfig := appconf.Load(appName, &config.AppConfig{}).(*config.AppConfig)
 	logger.SetUpLogger(appConfig.Logger.LogLevel)
-	fmt.Println(appConfig)
 
 	s := server.GRPCServer{}
 	s.Start(appConfig.GRPC)
